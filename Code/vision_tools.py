d2c929b1ba4fe2249be198d731a4cd4113b5b07c
TomBlackroad <mbl.7d@live.com>
2020-10-20 20:26:39 +0200

    camera issues

--- Code/vision_tools.py
+++ Code/vision_tools.py
@@ -172,9 +172,9 @@ class Vision:
 ###############################################################################
 
 	def processData(self, data, confi):
-		processed_data = 3*applyConfiFilter(data, confi)
+		processed_data = 3*self.applyConfiFilter(data, confi)
 		print(processed_data)
-		processed_data += applyDownsampleFilter(data, confi)		
+		processed_data += self.applyDownsampleFilter(data, confi)		
 		print(processed_data)
 		processed_data /= 4
 		print(processed_data)
