20099fd4ef36a16f15e34a5a457eee103ab7a8c5
Tom Blackroad <mbl.49d@gmail.com>
2020-12-27 18:03:25 +0100

    hello AI

--- Code/IMU.py
+++ Code/IMU.py
@@ -34,11 +34,16 @@ class IMU:
 
 
 	def read_raw_data(self, addr):
-		self.bus.write_byte_data(self.Device_Address,0xA5,0x5A)
+		#self.bus.write_byte_data(self.Device_Address,0xA5,0x5A)
 		#Accelero and Gyro value are 16-bit
-		high = self.bus.read_byte_data(self.Device_Address, addr)
-		low = self.bus.read_byte_data(self.Device_Address, addr+1)
+		#high = self.bus.read_byte_data(self.Device_Address, addr)
+		#low = self.bus.read_byte_data(self.Device_Address, addr+1)
 
+		
+
+		Register = self.bus.read_i2c_block_data(addr, addr+1,1)
+		high = Register[0]*1.0
+		low = Register[1]*1.0
 		print(high)
 		print(low)
 		#concatenate higher and lower value
